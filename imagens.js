53c28b0d16fc3a910a70ee426cec964ffb9dc161
WellingtonDePaula <wellingtonespiao@gmail.com>
2023-07-25 13:45:52 -0300

    adicionando sons

--- imagens.js
+++ imagens.js
@@ -1,8 +1,13 @@
+//imagens e sons do jogo
 var imagemDaEstrada;
 var imagemDoAtor;
 var imagemDoCarro1;
 var imagemDoCarro2;
 var imagemDoCarro3;
+//sons do jogo
+var somDaTrilha;
+var somColidiu;
+var somPonto;
 
 function preload() {
     imagemDaEstrada = loadImage("imagens/estrada.png");
@@ -11,5 +16,9 @@ function preload() {
     imagemDoCarro2 = loadImage("imagens/carro-2.png");
     imagemDoCarro3 = loadImage("imagens/carro-3.png");
     imagemCarros = [imagemDoCarro1, imagemDoCarro2, imagemDoCarro3, imagemDoCarro1, imagemDoCarro2, imagemDoCarro3];
+
+    somDaTrilha = loadSound("sons/trilha.mp3");
+    somColidiu = loadSound("sons/colidiu.mp3");
+    somPonto = loadSound("sons/pontos.wav");
 }
 
